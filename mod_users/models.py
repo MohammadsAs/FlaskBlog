4ccf76f9a39d0065d9b72f9e7fb1bee9195e2216
mohammadsala <mohammadsalaee@gmail.com>
2024-09-05 12:49:19 +0330

    Add "User.set_password" method to generate password hash

--- mod_users/models.py
+++ mod_users/models.py
@@ -1,3 +1,4 @@
+from werkzeug.security import generate_password_hash
 from sqlalchemy import Column, Integer, String
 from app import db
 
@@ -6,6 +7,9 @@ class User(db.Model):
     __tablename__= 'users'
     id = Column(Integer(), primary_key=True)
     email = Column(String(128), nullable=False, unique=True)
-    password = Column(String(128), nullable=False, unique=False)
+    password = Column(String(256), nullable=False, unique=False)
     role = Column(Integer(), nullable=False, default=0)
     full_name = Column(String(128), nullable=True, unique=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
